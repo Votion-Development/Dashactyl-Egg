31470e3e38b4018342a50987e5825a678e7112f4
Shersha <87840878+Shersha01@users.noreply.github.com>
2022-01-26 17:24:14 +0530

    Removed useless thing
    I guess we don't need this now.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /home/container || exit 1
-INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
-export INTERNAL_IP
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%sWelcome to Dashactyl"
 /dashactyl.sh
